6a7d8de3b409f11eae30ad966c0a4fd70e6926c2
Gabriel Sá <t6w311@gmail.com>
2024-01-28 17:52:23 -0300

    feat: add simulation service

--- src/application.py
+++ src/application.py
@@ -1,3 +1,5 @@
+from src.service.simulationService import handleSimulation
+
 import json
 import os
 
@@ -13,41 +15,4 @@ def loadSettings() -> dict:
 
 if __name__ == "__main__":
     settings = loadSettings()
-    
-
-
-# world = World()
-# worldSize = 3000
-
-# generateWorld(world, worldSize)
-
-# creature = generateCreatureWithGenome(5, 5)
-# for gene in creature.genome:
-#     print(gene)
-#     print(decodeGene(gene))
-
-
-# insertCreature(world, int(worldSize/2), int(worldSize/2), creature)
-# printWorld(world)
-# print(world.population)
-
-# os.system("cls")
-# printWorld(world)
-# paintWorld(world)
-# time.sleep(1)
-
-# for i in range(0, 60):
-#     acNeuronService.doAction(world, creature, acNeuronService.Actions.MOVE_RANDOM)
-#     os.system("cls")
-#     paintWorld(world, False)
-
-
-
-# sensoryNeurons = generateInputNeurons()
-# actionNeurons = generateActionNeurons()
-# for sensoryNeuron in sensoryNeurons:
-#     print(sensoryNeuron.name)
-# for actionNeuron in actionNeurons:
-#     print(actionNeuron.name)
-
-
+    handleSimulation(settings)
