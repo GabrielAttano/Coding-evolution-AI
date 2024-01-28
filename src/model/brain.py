06a7b44e74f573055c3475850165b5e4cb723eef
Gabriel Sá <t6w311@gmail.com>
2024-01-28 17:15:57 -0300

    feat: add brain

---
+++ src/model/brain.py
@@ -0,0 +1,6 @@
+class Brain:
+    def __init__(self) -> None:
+        self.sensoryNeurons = list()
+        self.intermediateNeurons = list()
+        self.actionNeurons = list()
+        self.connections = list()
