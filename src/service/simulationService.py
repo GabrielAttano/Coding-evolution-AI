8cc7fd13e832a2e4bbdf1d7f09da67c36a53b900
Gabriel Sá <t6w311@gmail.com>
2024-01-28 18:28:15 -0300

    feat: add better debug and inesrt creatures in world function

--- src/service/simulationService.py
+++ src/service/simulationService.py
@@ -1,7 +1,7 @@
 from model.world import World
 from model.creature import Creature
 
-from service.worldService import generateWorld
+from service.worldService import generateWorld, insertCreatureRandomPosition
 from service.creatureService import generateCreatureWithGenome
 from service.geneticsService import generateActionNeurons, generateInputNeurons, generateIntermediateNeurons
 
@@ -11,29 +11,47 @@ def handleSimulation(settings: dict):
     isDebug = settings["debug"]
 
     if isDebug:
+        print("=================================")
         print("Debug mode activated.")
+        print("=================================")
         print("World settings: " + str(worldSettings))
         print("Creature settings: " + str(creatureSettings))
 
     # ========== Creating world ==========
-    if isDebug: print("Creating world. Size: " + str(worldSettings["worldSize"]))
+    if isDebug:
+        print("=================================")
+        print("Creating world. Size: " + str(worldSettings["worldSize"]))
+        
     world = World()
     generateWorld(world, worldSettings["worldSize"])
 
     if isDebug: print("Finished creating world.")
 
     # ========== Populating world ==========
-    if isDebug: print("Generating creatures. Total: " + str(worldSettings["startPopulation"]))
+    if isDebug: 
+        print("=================================")
+        print("Generating creatures. Total: " + str(worldSettings["startPopulation"]))
     creatures = list()
     for i in range(worldSettings["startPopulation"]):
         creatures.append(generateCreatureWithGenome(creatureSettings))
 
     if isDebug: print("Finished generating creatures. Total: " + str(len(creatures)))
 
+    if isDebug: print("Inserting creatures into world randomly.")
+    for creature in creatures:
+        insertCreatureRandomPosition(world, creature)
+    
+    if isDebug: print("Finished inserting creatures. World population: " + str(world.population))
+
     # ========== load neurons ==========
-    if isDebug: print("Loading neurons")
+    if isDebug:
+        print("=================================")
+        print("Loading neurons")
 
     sensoryNeurons = generateInputNeurons()
-    intermediateNeurons = generateIntermediateNeurons(creatureSettings["innerNeurons"])
     actionNeurons = generateActionNeurons()
-    
+
+    if isDebug:
+        print("Finished loading neurons.")
+        print("Total sensory neurons: " + str(len(sensoryNeurons)))
+        print("Total action neurons: " + str(len(actionNeurons)))
