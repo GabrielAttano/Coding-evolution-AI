2f724027f5c454233e87d8d352abbebc7d900bf3
Gabriel Sá <t6w311@gmail.com>
2024-01-28 17:16:07 -0300

    feat: add brain to creature

--- src/model/creature.py
+++ src/model/creature.py
@@ -1,3 +1,5 @@
+from src.model.brain import Brain
+
 from enum import Enum
 
 class facingDirection(Enum):
@@ -10,14 +12,16 @@ class Creature:
     def __init__(self, genomeLength: int = 4, innerNeurons: int = 1) -> None:
         self.genomeLength = genomeLength
         self.innerNeurons = innerNeurons
-        self.responsiveness = 1.0
         self.genome = list()
+        
+        self.responsiveness = 1.0
+        
+        self.brain = Brain()
 
         self.age = 0
         self.maxAge = 10
 
         self.positionX = 0
         self.positionY = 0
-        
         self.facing = facingDirection.UP
         
