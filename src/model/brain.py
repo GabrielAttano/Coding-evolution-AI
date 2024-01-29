273ec073f3aabdf0d544d06a41fef622973f666f
Gabriel Sá <t6w311@gmail.com>
2024-01-29 15:30:03 -0300

    feat: add Connection class

--- src/model/brain.py
+++ src/model/brain.py
@@ -1,6 +1,14 @@
+from model.genetics import Neuron
+
 class Brain:
     def __init__(self) -> None:
         self.sensoryNeurons = list()
         self.intermediateNeurons = list()
         self.actionNeurons = list()
         self.connections = list()
+
+class Connection:
+    def __init__(self) -> None:
+        self.sourceNeuron: Neuron = None
+        self.sinkNeuron: Neuron = None
+        self.weight: float = 0
