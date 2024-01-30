fb68b0b9d6d5f4b58c2a0cabdd4983034ac66d56
Gabriel Sá <t6w311@gmail.com>
2024-01-30 16:18:57 -0300

    feat: add create video

--- src/service/worldService.py
+++ src/service/worldService.py
@@ -5,6 +5,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import cv2
 import random
+import os
 
 def generateWorld(world: World, worldSize: int):
         world.worldSize = worldSize
@@ -36,19 +37,29 @@ def printWorld(world: World):
             print(" ", end="")
         print("\n")
 
-def paintWorld(world: World, saveMode: bool):
+def paintWorld(world: World, saveMode: bool, inMemoryFrames: list):
     blank_image = np.ones((world.worldSize, world.worldSize, 3), dtype=np.uint8) * 255
 
     for row in reversed(range(world.worldSize)):
         for col in range(world.worldSize):
             if world.cells[row][col].isCreature:
                 x, y = row, col
-                cv2.circle(blank_image, (y, x), radius = 2, color=(0, 0, 255), thickness=-1)
+                cv2.circle(blank_image, (y, x), radius = 5, color=(0, 0, 255), thickness=-1)
 
-    if not saveMode:
-        plt.imshow(blank_image)
-        plt.axis('off')
-        plt.show()
+    # Save the image
+    if saveMode:
+        inMemoryFrames.append(blank_image.copy())
+
+def createVideo(frames: list):
+    height, width, _ = frames[0].shape
+    size = (width, height)
+
+    out = cv2.VideoWriter('output_video.avi', cv2.VideoWriter_fourcc(*'DIVX'), 60, size)
+
+    for i in range(len(frames)):
+        out.write(frames[i])
+
+    out.release()
 
 def insertCreature(world: World, posX: int, posY: int, creature: Creature):
     cell: CellData = world.cells[posY][posX]
