cdc15d00e41f5db0943a3b5da4ebde4a2bce57e8
Gabriel Sá <t6w311@gmail.com>
2024-02-04 20:11:13 -0300

    chore: remove unused code

--- src/application.py
+++ src/application.py
@@ -4,16 +4,6 @@ from service.settingsService import SettingsHandler
 import json
 import os
 
-def loadSettings() -> dict:
-    scriptPath = os.path.abspath(__file__)
-    configuration_path = os.path.join(os.path.dirname(scriptPath), 'simulationSettings.json')
-
-    settings = dict()
-    with open(configuration_path, 'r') as file:
-        settings = json.load(file)
-    
-    return settings
-
 if __name__ == "__main__":
     settingsHandler = SettingsHandler()
     handleSimulation(settingsHandler)
